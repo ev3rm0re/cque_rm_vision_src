b5b3c4f42ed4b87d04491720b8fd4f3f31566be3
ev3rm0re <ev3rm0re@163.com>
2024-02-29 12:45:02 +0800

    02.29

--- rm_serial_driver/include/rm_serial_driver/packet.hpp
+++ rm_serial_driver/include/rm_serial_driver/packet.hpp
@@ -16,12 +16,12 @@ struct ReceivePacket
   uint8_t detect_color : 1;  // 0-red 1-blue
   bool reset_tracker : 1;
   uint8_t reserved : 6;
-  float roll;
-  float pitch;
-  float yaw;
-  float aim_x;
-  float aim_y;
-  float aim_z;
+  // float roll;
+  // float pitch;
+  // float yaw;
+  // float aim_x;
+  // float aim_y;
+  // float aim_z;
   uint16_t checksum = 0;
 } __attribute__((packed));
 
