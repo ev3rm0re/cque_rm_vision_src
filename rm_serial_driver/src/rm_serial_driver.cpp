c97972638bb70b171bbce9a8abb6076bd4930e88
ev3rm0re <ev3rm0re@163.com>
2024-03-12 22:07:40 +0800

    03.12

--- rm_serial_driver/src/rm_serial_driver.cpp
+++ rm_serial_driver/src/rm_serial_driver.cpp
@@ -164,7 +164,7 @@ void RMSerialDriver::sendData(const auto_aim_interfaces::msg::Target::SharedPtr 
     packet.armors_num = msg->armors_num;
     packet.yaw = atan2(msg->position.y, msg->position.x);
     packet.distance = msg->position.x;
-    if (abs(packet.yaw) < 0.1 || packet.id == 0) {
+    if (abs(packet.yaw) < 0.01 || packet.id == 0) {
       packet.yaw = 0.0;
     }
     // packet.x = msg->position.x;
@@ -184,7 +184,7 @@ void RMSerialDriver::sendData(const auto_aim_interfaces::msg::Target::SharedPtr 
 
     serial_driver_->port()->send(data);
 
-    // RCLCPP_INFO(get_logger(), "yaw: %f, distance: %f", packet.yaw, packet.distance);
+    RCLCPP_INFO(get_logger(), "yaw: %f, distance: %f", packet.yaw, packet.distance);
 
     std_msgs::msg::Float64 latency;
     latency.data = (this->now() - msg->header.stamp).seconds() * 1000.0;
